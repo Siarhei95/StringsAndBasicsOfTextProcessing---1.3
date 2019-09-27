27a46736673380d7c8fa2c10a9e00cc1070ea187
asus <s.pashkovskiy08@gmail.com>
2019-09-27 14:05:07 +0300

    StringsAndBasicsOfTextProcessing---1.3

---
+++ src/Question_3/question_3.java
@@ -0,0 +1,14 @@
+package Question_3; //В строке найти количество цифр
+
+public class question_3 {
+    public static void main(String[] args) {
+        String someWords = ";1-car; 2-motors; 30 - bicycles ant etc.";
+        int count = 0;
+            for (int i = 0; i < someWords.length(); i++) {
+                if (Character.isDigit(someWords.charAt(i))){ //через метод Character.isDigit() — определим, является ли указанное значение типа char цифрой.
+                    count++;                                 //метод charAt() — возвратит символ, расположенный по указанному индексу (i) строки.
+                }
+            }
+            System.out.println("Digits per line: " + count);
+        }
+    }
